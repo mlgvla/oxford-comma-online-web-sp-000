08519f5d9ac655f0dd397264aeede5070df8546c
Monica Gerard <mlgerardvla@gmail.com>
2020-03-12 20:08:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
+require 'pry'
 def oxford_comma(array)
+  new_string = ""
 
+  case array.length
+  when 1
+    return new_string << array[0]
+  when 2
+    return new_string << "#{array[0]} and #{array[1]}"
+  else
+    array.each do |element|
+      if array.first
+        new_string << element   #if first, shovel onto new_string
+      elsif array.last
+        new_string << ", and " << element   #elsif last, shovel ", and  " then shovel elemnent
+      else
+        binding.pry
+        new_string << ", " << element     #else in between, shovel ", " then shovel element
+      end
+    end
+  end
+  return new_string
 end
+
+sample_array = ["one","two","three"]
+oxford_comma(sample_array)
