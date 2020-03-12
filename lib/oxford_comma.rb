237d8b6185d7e35084c7b474f0274e6fbdfdbc10
Monica Gerard <mlgerardvla@gmail.com>
2020-03-12 20:40:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
+require 'pry'
 def oxford_comma(array)
+  new_string = ""
 
+  case array.length
+  when 1
+    return new_string << array[0]
+  when 2
+    return new_string << "#{array[0]} and #{array[1]}"
+  else
+    array.each do |element|
+        if array[array.length - 1] == element
+          new_string << "and "<< element
+        else
+          new_string << element << ", "
+        end
+      end
+  end
+  return new_string
 end
+
+#sample_array = ["one","two","three"]
+#oxford_comma(sample_array)
